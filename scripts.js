cf0d6c5ca4a1ab9e0860d870d793885b88f09a0b
mkdesign <me@mkdesign.ir>
2012-10-07 20:09:14 +0330

    index finished

--- scripts.js
+++ scripts.js
@@ -1,23 +1,40 @@
 $(function(){
 
-	var clickHide = $('#hide-menu a.click-hide'),
+	var clickHide   = $('#hide-menu a.click-hide'),
 		menuWrapper = $('aside.menu-wrapper'),
-		clickImg = $('#hide-menu a.click-hide img');
-	clickHide.click(function(){
-		menuWrapper.animate({
-			top:-520
-		},600,'ease-out',function(){
-			clickImg.animate({
-				rotate : -180+'deg'
-			},300).click(function(){
+		clickImg    = $('#hide-menu a.click-hide img');
+
+	
+
+		clickHide.click(function(){
+
+			if(menuWrapper.css('top')==0+'px'){
+
 				menuWrapper.animate({
-					top:0
+					
+						top:-520
+					
 				},600,'ease-out',function(){
+
 					clickImg.animate({
-						rotate : 180+'deg'
-					},300);
-				});	
-			});
+
+						rotate : -180+'deg'
+
+					},250);
+				});
+			}else{
+				menuWrapper.animate({
+					
+						top:0
+					
+				},600,'ease-out',function(){
+
+					clickImg.animate({
+
+						rotate : 0
+
+					},250);
+				});
+			}
 		});
-	});
 })
