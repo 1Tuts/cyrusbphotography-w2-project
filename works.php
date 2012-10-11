2c2593e021404f29fe54d1a7c8194e70e241ccf7
Aidin Toussi <aidin.toussi@gmail.com>
2012-10-11 21:31:05 +0330

    some little changes

--- works.php
+++ works.php
@@ -48,9 +48,39 @@
 						</div>
 						
 					</li>
-					<li></li>
-					<li></li>
-					<li></li>
+					<li>
+						<h3>
+							<a href="images/work-big.jpg">عنوان عکس</a>							
+						</h3>
+						<div class="image-holder">
+							<a class="gallery-image" href="images/work-big.jpg"> <!--link to big image-->
+								<img src="images/works/1.png" width="320" height="220"/>
+							</a>
+						</div>
+						
+					</li>
+					<li>
+						<h3>
+							<a href="images/work-big.jpg">عنوان عکس</a>							
+						</h3>
+						<div class="image-holder">
+							<a class="gallery-image" href="images/work-big.jpg"> <!--link to big image-->
+								<img src="images/works/1.png" width="320" height="220"/>
+							</a>
+						</div>
+						
+					</li>
+					<li>
+						<h3>
+							<a href="images/work-big.jpg">عنوان عکس</a>							
+						</h3>
+						<div class="image-holder">
+							<a class="gallery-image" href="images/work-big.jpg"> <!--link to big image-->
+								<img src="images/works/1.png" width="320" height="220"/>
+							</a>
+						</div>
+						
+					</li>
 				</ul>
 			</div>
 		</div>
