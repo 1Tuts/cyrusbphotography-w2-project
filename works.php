9bf68bbac53385832a871a84be9e79a8c2edbf63
Aidin Toussi <aidin.toussi@gmail.com>
2012-10-11 18:30:41 +0330

    create works.php page / copy from index.php

---
+++ works.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" href="reset.css" />
+	<link rel="stylesheet" href="style.css" />
+	<script type="text/javascript" src="zepto.min.js"></script>
+	<script type="text/javascript" src="back-slide.js"></script>
+	<script type="text/javascript" src="scripts.js"></script>
+	<title></title>
+</head>
+<body>
+	<div class="container">
+		<aside class="sidebar">
+			<header>
+				<hgroup>
+					<h1></h1>
+					<h1></h1>	
+				</hgroup>
+				<nav>
+					<menu>
+						<li><a href="#">home<br /> <span>back to home</span></a></li>
+						<li><a href="#">About me<br /> <span>who am i ?</span></a></li>
+						<li><a href="#">portfolio<br /> <span>my works</span></a></li>
+						<li><a href="#">cooprate<br /> <span>works with frinds</span></a></li>
+						<li><a href="#">contact me<br /> <span>ways to find me</span></a></li>
+					</menu>
+				</nav>
+			</header>
+			<div id="hide-sidebar">
+				<a href="#" class="click-hide">
+					<img src="images/menu_hide_arrow_top.png" alt="click-hide" />
+				</a>
+			</div>
+		</aside>
+		<div class="content">
+			<h1 class="title">نمونه کارها</h1>
+		</div>
+	</div>
+</body>
+</html>
