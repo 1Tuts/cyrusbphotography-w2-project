23ee12376588da49bfaa0944a2fcdc3e642f183a
Aidin Toussi <aidin.toussi@gmail.com>
2012-10-16 23:24:46 +0330

    create social network icons

--- works.php
+++ works.php
@@ -128,14 +128,14 @@
 				<div class="copyright">
 					<section class="social">
 						<ul>
-							<li><a href="#"><img src="images/#" width="16" height="16"/></a></li>
-							<li><a href="#"><img src="images/#" width="16" height="16"/></a></li>
-							<li><a href="#"><img src="images/#" width="16" height="16"/></a></li>
-							<li><a href="#"><img src="images/#" width="16" height="16"/></a></li>
-							<li><a href="#"><img src="images/#" width="16" height="16"/></a></li>
-							<li><a href="#"><img src="images/#" width="16" height="16"/></a></li>
-							<li><a href="#"><img src="images/#" width="16" height="16"/></a></li>
-							<li><a href="#"><img src="images/#" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/facebook.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/googleplus.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/linkedin.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/twitter.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/flickr.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/picasa.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/youtube.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/rss.png" width="16" height="16"/></a></li>
 						</ul>
 					</section>
 					<p>copyright 2012 sirous bijanfar</p>
