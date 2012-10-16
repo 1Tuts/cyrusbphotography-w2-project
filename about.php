ba2ebefe7bc9d69660a23ed115544fc0ad6f2405
Aidin Toussi <aidin.toussi@gmail.com>
2012-10-17 00:11:12 +0330

    insert footer in about page and adjust header text size and setting

--- about.php
+++ about.php
@@ -53,10 +53,64 @@
 		<div class="content">
 			<h1 class="title">درباره من</h1>
 			<h1 class="cyrus">سیروس بیژن فر</h1>
-			<p class="biography">
-				متولد سال ۱۳۶۰ در مشهد.تحصیلات دانشگاهی در رشته عکاسی از دانشگاه تهران.
-			</p>
-
+			<section class="biography">
+				<p>
+					متولد سال ۱۳۶۰ در مشهد.تحصیلات دانشگاهی در رشته عکاسی از دانشگاه تهران.
+				</p>
+				<p>
+					متولد سال ۱۳۶۰ در مشهد.تحصیلات دانشگاهی در رشته عکاسی از دانشگاه تهران.
+				</p>
+				<p>
+					متولد سال ۱۳۶۰ در مشهد.تحصیلات دانشگاهی در رشته عکاسی از دانشگاه تهران.
+				</p>
+				<p>
+					متولد سال ۱۳۶۰ در مشهد.تحصیلات دانشگاهی در رشته عکاسی از دانشگاه تهران.
+				</p>
+			</section>
+			<footer class="footer">
+				<div class="boxes">
+					<div class="box">
+						<h3>تیتر آزمایشی</h3>
+						<ul>
+							<li><a href="#">آیتم یک</a></li>
+							<li><a href="#">آیتم دو</a></li>
+							<li><a href="#">آیتم سه</a></li>
+						</ul>
+					</div>
+					<div class="box">
+						<h3>تیتر آزمایشی</h3>
+						<ul>
+							<li><a href="#">آیتم یک</a></li>
+							<li><a href="#">آیتم دو</a></li>
+							<li><a href="#">آیتم سه</a></li>
+						</ul>
+					</div>
+					<div class="box">
+						<h3>تیتر آزمایشی</h3>
+						<ul>
+							<li><a href="#">آیتم یک</a></li>
+							<li><a href="#">آیتم دو</a></li>
+							<li><a href="#">آیتم سه</a></li>
+						</ul>
+					</div>
+					<div class="clear"></div>
+				</div>
+				<div class="copyright">
+					<section class="social">
+						<ul>
+							<li><a href="#" target="_blank"><img src="images/facebook.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/googleplus.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/linkedin.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/twitter.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/flickr.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/picasa.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/youtube.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/rss.png" width="16" height="16"/></a></li>
+						</ul>
+					</section>
+					<p>copyright 2012 sirous bijanfar</p>
+				</div>
+			</footer>
 		</div>
 	</div>
 </body>
