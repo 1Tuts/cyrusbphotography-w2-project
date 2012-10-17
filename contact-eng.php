57b8087942bdf051c9d2a0af0d74667f45c3a878
mkdesign <me@mkdesign.ir>
2012-10-17 23:13:29 +0330

    a little mistake in tags

--- contact-eng.php
+++ contact-eng.php
@@ -68,51 +68,51 @@
 						<li class="google-map"></li>
 					</ul>
 				</div>
-				<footer class="footer">
-					<div class="boxes">
-						<div class="box">
-							<h3>تیتر آزمایشی</h3>
-							<ul>
-								<li><a href="#">آیتم یک</a></li>
-								<li><a href="#">آیتم دو</a></li>
-								<li><a href="#">آیتم سه</a></li>
-							</ul>
-						</div>
-						<div class="box">
-							<h3>تیتر آزمایشی</h3>
-							<ul>
-								<li><a href="#">آیتم یک</a></li>
-								<li><a href="#">آیتم دو</a></li>
-								<li><a href="#">آیتم سه</a></li>
-							</ul>
-						</div>
-						<div class="box">
-							<h3>تیتر آزمایشی</h3>
-							<ul>
-								<li><a href="#">آیتم یک</a></li>
-								<li><a href="#">آیتم دو</a></li>
-								<li><a href="#">آیتم سه</a></li>
-							</ul>
-						</div>
-						<div class="clear"></div>
-					</div>
-					<div class="copyright">
-						<section class="social">
-							<ul>
-								<li><a href="#" target="_blank"><img src="images/facebook.png" width="16" height="16"/></a></li>
-								<li><a href="#" target="_blank"><img src="images/googleplus.png" width="16" height="16"/></a></li>
-								<li><a href="#" target="_blank"><img src="images/linkedin.png" width="16" height="16"/></a></li>
-								<li><a href="#" target="_blank"><img src="images/twitter.png" width="16" height="16"/></a></li>
-								<li><a href="#" target="_blank"><img src="images/flickr.png" width="16" height="16"/></a></li>
-								<li><a href="#" target="_blank"><img src="images/picasa.png" width="16" height="16"/></a></li>
-								<li><a href="#" target="_blank"><img src="images/youtube.png" width="16" height="16"/></a></li>
-								<li><a href="#" target="_blank"><img src="images/rss.png" width="16" height="16"/></a></li>
-							</ul>
-						</section>
-						<p>copyright 2012 sirous bijanfar</p>
-					</div>
-				</footer>
 			</div>
+			<footer class="footer">
+				<div class="boxes">
+					<div class="box">
+						<h3>تیتر آزمایشی</h3>
+						<ul>
+							<li><a href="#">آیتم یک</a></li>
+							<li><a href="#">آیتم دو</a></li>
+							<li><a href="#">آیتم سه</a></li>
+						</ul>
+					</div>
+					<div class="box">
+						<h3>تیتر آزمایشی</h3>
+						<ul>
+							<li><a href="#">آیتم یک</a></li>
+							<li><a href="#">آیتم دو</a></li>
+							<li><a href="#">آیتم سه</a></li>
+						</ul>
+					</div>
+					<div class="box">
+						<h3>تیتر آزمایشی</h3>
+						<ul>
+							<li><a href="#">آیتم یک</a></li>
+							<li><a href="#">آیتم دو</a></li>
+							<li><a href="#">آیتم سه</a></li>
+						</ul>
+					</div>
+					<div class="clear"></div>
+				</div>
+				<div class="copyright">
+					<section class="social">
+						<ul>
+							<li><a href="#" target="_blank"><img src="images/facebook.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/googleplus.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/linkedin.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/twitter.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/flickr.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/picasa.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/youtube.png" width="16" height="16"/></a></li>
+							<li><a href="#" target="_blank"><img src="images/rss.png" width="16" height="16"/></a></li>
+						</ul>
+					</section>
+					<p>copyright 2012 sirous bijanfar</p>
+				</div>
+			</footer>
 		</div>
 	</div>
 </body>
