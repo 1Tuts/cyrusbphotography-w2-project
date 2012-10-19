febacdc38b98a5f58a4d9ad549a7c3884a9a2e8e
Aidin Toussi <aidin.toussi@gmail.com>
2012-10-19 13:39:59 +0330

    delete some null spaces

--- scripts.js
+++ scripts.js
@@ -1,42 +1,23 @@
 $(function(){
-
 	var clickHide   = $('#hide-sidebar a.click-hide'),
 		menuWrapper = $('aside.sidebar'),
 		clickImg    = $('#hide-sidebar a.click-hide img');
 
-	
-
-	
-
-	
-
 		clickHide.click(function(){
-
 			if(menuWrapper.css('top')==0+'px'){
-
-				menuWrapper.animate({
-					
+				menuWrapper.animate({	
 						top:-500
-					
 				},600,null,function(){
-
 					clickImg.animate({
-
 						rotate : -180+'deg'
-
 					},250);
 				});
 			}else{
-				menuWrapper.animate({
-					
-						top:0
-					
+				menuWrapper.animate({		
+						top:0		
 				},600,null,function(){
-
 					clickImg.animate({
-
 						rotate : 0
-
 					},250);
 				});
 			}
