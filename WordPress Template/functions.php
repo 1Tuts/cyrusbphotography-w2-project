c91d634e335992fe850a6132d953e6a5dde66e08
Aidin Toussi <aidin.toussi@gmail.com>
2012-10-27 14:54:17 +0330

    insert functions elements

--- WordPress Template/functions.php
+++ WordPress Template/functions.php
@@ -3,6 +3,20 @@
 	add_theme_support('menus');
 	add_action('init','post_init');
 		function blog_init(){
-		}
+			$labels = array(
+			'name' => _x('بلاگ','post type general name'),
+			'singular_name' => _x('پست','post type singular name'),
+			'add new' => _x('افزودن' , 'بلاگ'),
+			'add_new_item' => ('پست جدید'),
+			'edit_item' => ('ویرایش مطلب'),
+			'new_item' => ('پست جدبد'),
+			'view_item' => ('مشاهده پست'),
+			'search_items' => ('جستجوی پست'),
+			'not_found' => ('هیچ پستی یافت نشد'),
+			'not_found_in_trash' => ('هیچ پستی در زباله دان یافت نشد'),
+			'parent_item_colon' => '',
+			'menu_name' => 'بلاگ'
+		);
+	}
 
 	register_nav_menu('sidebar menu','Main Menu');
